c6d485985549e0e4500caa7591ba3185e841b455
Georges Simon <simgeorges000@gmail.com>
2023-04-23 13:34:54 +0200

    Added Bike model

---
+++ src/model/Bike.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Bike {
+    private String bikeID;
+    private String model;
+    private String brand;
+    private LocalDate purchaseDate;
+    private Double value;
+    private Boolean isDeclassified;
+
+    public Bike(String bikeID, String model, String brand, LocalDate purchaseDate, Double value, Boolean isDeclassified) {
+        setBikeID(bikeID);
+        setModel(model);
+        setBrand(brand);
+        setPurchaseDate(purchaseDate);
+        setValue(value);
+        setDeclassified(isDeclassified);
+    }
+
+    public void setBikeID(String bikeID) {
+        this.bikeID = bikeID;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public void setDeclassified(Boolean declassified) {
+        isDeclassified = declassified;
+    }
+
+    public String getBikeID() {
+        return bikeID;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+}
