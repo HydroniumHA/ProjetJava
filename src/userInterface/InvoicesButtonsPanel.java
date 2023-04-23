25ef40c1034ffa5730630ea410091b9a31e67b7f
Georges Simon <simgeorges000@gmail.com>
2023-04-23 13:41:35 +0200

    Import modification

--- src/userInterface/InvoicesButtonsPanel.java
+++ src/userInterface/InvoicesButtonsPanel.java
@@ -4,8 +4,7 @@ import controller.ApplicationController;
 import model.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class InvoicesButtonsPanel extends JPanel {
     private JButton back, validation, reset;
