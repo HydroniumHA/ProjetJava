588230961a629f15a7f3587746d3f5ddadee1d9b
Georges Simon <simgeorges000@gmail.com>
2023-05-15 17:14:03 +0200

    Information about the program & dev in help menu

--- src/userInterface/AboutInformation.java
+++ src/userInterface/AboutInformation.java
@@ -9,7 +9,14 @@ public class AboutInformation extends JPanel {
     private JButton back;
 
     public AboutInformation() {
-        texte = new JLabel("<html>INFORMATIONS A MODIF</html>");
+        texte = new JLabel(
+                "<html><h1>Software Information<h2>Libiavelo</h2><p>Version : V63<br>Developer : Simon Georges<br>Contact : simgeorges000@gmail.com +32460946755<br>Release Date : 17/05/23"
+                + "<br><br><h2>There are 5 menus with submenus.</h2><br>In the Customer menu, you can register, delete or modify a customer's information."
+                + "<br>The management menu (JobTask) is used to display the average monthly repair time of the bicycles in each building."
+                + "<br>The query menu is used to search:"
+                + "<br>&emsp;-All invoices above a certain amount for a given city."
+                + "<br>&emsp;-The repair number of bicycles being repaired in a given building."
+                + "<br>&emsp;-An address from a card number.</p></html>");
         texte.setHorizontalAlignment(SwingConstants.CENTER);
         this.setLayout(new BorderLayout());
         this.add(texte, BorderLayout.CENTER);
